566f2857fcb7a2c4ded8c544ef3684de3a6c367e
Faisal Rana <inquisitive@gmail.com>
2021-08-24 20:58:05 -0700

    Updates

---
+++ precache-manifest.ea831094d0af7506c795fb9f69d3a0f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6030f295bcc9859e50d47fcc38c943cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08420454a99f6d9d0d70",
+    "url": "/static/css/main.19a23e62.chunk.css"
+  },
+  {
+    "revision": "af8d257d33072f3f6cc9",
+    "url": "/static/js/2.601d76bb.chunk.js"
+  },
+  {
+    "revision": "08420454a99f6d9d0d70",
+    "url": "/static/js/main.75983f75.chunk.js"
+  },
+  {
+    "revision": "d3d41e1de46093cb7ed1",
+    "url": "/static/js/runtime-main.63a8b8f8.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "00e5f853f5db44d819027c8803c279ce",
+    "url": "/static/media/pepsiSpace.00e5f853.png"
+  },
+  {
+    "revision": "800ed3223e2824a1908569da2b881b43",
+    "url": "/static/media/pepsiSpace1.800ed322.png"
+  }
+]);
